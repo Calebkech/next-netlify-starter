46e9d4370c158f02d4bb1406644b0ca83a55dbc4
caleb.kech <37027115+Calebkech@users.noreply.github.com>
2023-12-17 20:10:37 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,19 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Caleb Kech" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title=" Caleb Kech" />
+        <p className=""><b>Reach The DevOps And Security Community</b></p> 
+        <p className="description"><i>
+          Authentic Engagement: 
+          We're not here to impress with jargon or elaborate buzzwords. 
+          Instead, let's engage in a conversation that speaks directly to the challenges you face. 
+          Our authentic voice will guide you through the intricacies of security and DevOps, 
+          shedding light on what truly makes a difference.</i>
         </p>
       </main>
 
