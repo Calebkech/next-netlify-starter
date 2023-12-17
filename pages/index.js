a89e3ee13e5fe3faac13929aecf8341e384b144e
caleb.kech <37027115+Calebkech@users.noreply.github.com>
2023-12-17 20:33:37 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,47 +9,10 @@ export default function Home() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="" />
-        <meta name="author" content="John Hammond LLC" />
+        <meta name="author" content="CALEB KECH LLC" />
         <title></title>
 
-        {/* Bootstrap */}
-        {/* <link href="https://getbootstrap.com/docs/5.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" /> */}
-        <link
-          href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
-        />
-
-        {/* Favicons */}
-        <meta name="theme-color" content="#712cf9" />
-
-        {/* FontAwesome */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
-        <script
-          type="text/javascript"
-          src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=RYHhqJbVBpqjh_RL7gPcCi2dsG4YDUf4vORtxlL3j2y44E_s3Z8GyHRfDiZe1tj4"
-          charset="UTF-8"
-        ></script>
-        <link
-          rel="stylesheet"
-          crossorigin="anonymous"
-          href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9qb2huaGFtbW9uZC5vcmcv"
-        />
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
-          integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        ></script>
-
-        {/* Custom styles for this template */}
+        {/* Include your local styles here */}
         <link href="/static/css/landing.css" rel="stylesheet" />
         <link href="/static/css/icon-colors.css" rel="stylesheet" />
       </Head>
@@ -57,19 +20,68 @@ export default function Home() {
       <div className="col-lg-8 mx-auto p-5 py-md-4">
         <header className="d-flex align-items-center pb-3 mb-5 border-bottom">
           <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
-            <span className="fs-4 text-muted">JOHN HAMMOND</span>
+            <span className="fs-4 text-muted">CALEB KECH</span>
           </a>
         </header>
 
         <main>
-          {/* Rest of your content */}
-        </main>
-
-        <footer className="pt-5 my-5 mb-1 text-muted border-top">John Hammond LLC &middot; &copy; 2023</footer>
+  <div className="row">
+    <div className="fs-5 col-md-8">
+      <h1>Reach The Security Community</h1>
+      <p>Inspire and educate security professionals without fluff.</p>
+      <p>Engage the industry with an authentic voice and show people what matters.</p>
+      <p>Share content, foster learning, and make your work known &mdash; <i>we'll do it together</i>.</p>
+      <br />
+      <div className="mb-5">
+        <a href="/sponsor" className="btn btn-primary btn-lg px-4">
+          Become a Partner
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="/bio" className="btn btn-info btn-lg px-4">
+          Learn about Caleb
+        </a>
       </div>
 
-      <script src="https://getbootstrap.com/docs/5.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-      <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/22389449.js"></script>
+      <div className="mb-5">
+        {/* Additional content goes here */}
+      </div>
+    </div>
+
+    <div id="jh-circle" className="col-md-4">
+      <img src="/static/img/jh_circle.png" />
+    </div>
+  </div>
+
+  <hr className="col-3 col-md-2 mb-4" />
+
+  <div className="row g-5">
+    <div className="col-md-6">
+      <h2>Tell the Story</h2>
+      <p>You control the narrative. Whatever you want to hear, whatever you want to say &mdash; be the subject matter expert across different domains.</p>
+      <ul className="icon-list">
+        <li className="mb-1">
+          <a href="https://www.youtube.com/playlist?list=PL1H1sBF1VAKW3w0itm6ha7uuqq11JBe4D">Security Professionals Insight & Interviews</a>
+        </li>
+
+        {/* Add more list items as needed */}
+
+      </ul>
+    </div>
+
+    <div className="col-md-6">
+      <h2>See the Impact</h2>
+      <p>Security education doesn't need to be behind barriers or gates. Break down the walls and bring your message to the community.</p>
+
+      {/* Add social media buttons or links here */}
+
+      <p>Need the news low-and-slow? Join the club.</p>
+      
+    </div>
+  </div>
+</main>
+
+        <footer className="pt-5 my-5 mb-1 text-muted border-top">Caleb Kech LLC &middot; &copy; 2023</footer>
+      </div>
     </>
   );
 }
